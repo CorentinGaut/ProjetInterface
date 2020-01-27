8986742f44640107b4a732fa2c469e564717b72c
Seransei <marrocq.alois@laposte.net>
2020-01-27 12:00:20 +0100

    Interactions UI fonctionnent
    TODO :
    - actions sur touches "S" et "G" 
    - map

--- TP_1_Interface/Assets/Scripts/GameManager.cs
+++ TP_1_Interface/Assets/Scripts/GameManager.cs
@@ -1,8 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    private string currentDisplay = "Start Menu";
+
+    public GameObject startMenu;
+    public GameObject instructionsMenu;
+
+    private void Awake()
+    {
+        DisplayStartMenu();
+    }
+
+    public void Play()
+    {
+        DisplayInstructionsMenu();
+        currentDisplay = "Instructions Menu";
+    }
+
+    public void Continue()
+    {
+        DisplayGame();
+        currentDisplay = "Game";
+    }
+
+    public void Menu()
+    {
+        DisplayStartMenu();
+        currentDisplay = "Start Menu";
+    }
+
+    private void DisplayStartMenu()
+    {
+        instructionsMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+
+    private void DisplayInstructionsMenu()
+    {
+        startMenu.SetActive(false);
+        instructionsMenu.SetActive(true);
+    }
+    private void DisplayGame()
+    {
+        instructionsMenu.SetActive(false);
+        startMenu.SetActive(false);
+    }
 }
